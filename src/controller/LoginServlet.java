3db5c50498fcd1c402a10511e37c36534ae70e4c
wangshuang <wangshuang@ciics.com>
2021-03-10 14:38:04 +0800

    第一个servlet

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -1,4 +1,20 @@
-package Controller;
+package controller;
 
-public class LoginServlet  {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doget");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("dopost");
+    }
 }
